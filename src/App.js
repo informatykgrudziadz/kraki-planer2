27e109a99472d2d9be0656b270bc25952df6b9c3
grudziadzinformatyk <grudziadzinformatyk@gmail.com>
2020-03-25 07:16:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="michal"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rutrum ac libero non
+        vulputate. Vivamus id nisl maximus, iaculis eros id, egestas est. Fusce consectetur
+        tristique elit a efficitur. Aliquam tempor risus eu aliquam sagittis. Morbi mattis molestie
+        odio. Proin viverra pulvinar ornare. Quisque sem ex, congue nec velit nec.
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
