911268b06cc88ff5f181be5b4b8f3a67a2701dc0
grudziadzinformatyk <grudziadzinformatyk@gmail.com>
2020-03-26 07:20:10 +0100

    test commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <a className="App-link" href="https://reactjs.org">
-        test
-      </a>
       <a
         className="App-link"
         href="https://reactjs.org"
